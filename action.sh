9336dd96198a9fee4714dec9b64a26bd3b501a6e
Prslc <prslc113@gmail.com>
2025-11-18 04:04:45 +0800

    opt: fmt

--- action.sh
+++ action.sh
@@ -1,6 +1,8 @@
+#!/system/bin/sh
 input="/sys/class/power_supply/battery/input_suspend"
+
 if [ $(cat $input) -eq 0 ]; then
- echo 1 > $input
+    echo 1 > $input
 else
- echo 0 > $input
+    echo 0 > $input
 fi
